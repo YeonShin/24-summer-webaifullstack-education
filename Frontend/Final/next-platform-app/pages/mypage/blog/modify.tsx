4ab6b589bc46847b908c7aeb258b7f94704c5fc8
김연신 <127587330+YeonShin@users.noreply.github.com>
2024-08-16 09:58:11 +0900

    미션 세팅

--- Frontend/Final/next-platform-app/pages/mypage/blog/modify.tsx
+++ Frontend/Final/next-platform-app/pages/mypage/blog/modify.tsx
@@ -1,56 +1,32 @@
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid';
-
-const Profile = () => {
+const BlogModify = () => {
   return (
     <form>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">
-            Profile
+            신규 게시글
           </h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            This information will be displayed publicly so be careful what you
-            share.
+            신규 게시글을 작성합니다.
           </p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="col-span-full">
-              <label
-                htmlFor="photo"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Photo
-              </label>
-              <div className="mt-2 flex items-center gap-x-3">
-                <UserCircleIcon
-                  className="h-12 w-12 text-gray-300"
-                  aria-hidden="true"
-                />
-                <button
-                  type="button"
-                  className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  Change
-                </button>
-              </div>
-            </div>
-
             <div className="sm:col-span-4">
               <label
                 htmlFor="username"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Username
+                글제목
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <input
-                    type="text"
-                    name="username"
                     id="username"
-                    autoComplete="username"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    name="username"
+                    type="text"
                     placeholder="janesmith"
+                    autoComplete="username"
+                    className="block flex-1 border-0  bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
@@ -61,7 +37,7 @@ const Profile = () => {
                 htmlFor="about"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                About
+                글내용
               </label>
               <div className="mt-2">
                 <textarea
@@ -72,9 +48,45 @@ const Profile = () => {
                   defaultValue={''}
                 />
               </div>
-              <p className="mt-3 text-sm leading-6 text-gray-600">
-                Write a few sentences about yourself.
-              </p>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                게시여부
+              </label>
+              <div className="mt-2">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                >
+                  <option>게시함</option>
+                  <option>게시안함</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                첨부파일
+              </label>
+              <div className="mt-2">
+                <input
+                  type="file"
+                  className="block border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400
+    file:bg-gray-50 file:border-0
+    file:me-4
+    file:py-3 file:px-4
+    dark:file:bg-neutral-700 dark:file:text-neutral-400"
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -98,4 +110,4 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default BlogModify;
